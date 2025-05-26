f43f63fd10b5f61eb44234196e943b7dc4eb6d0f
Guilherme Ferreira <guilherme-53@hotmail.com>
2025-05-26 20:58:24 -0300

    Função para definir as opções do menu

---
+++ cadastro_user.py
@@ -0,0 +1,9 @@
+usuarios = []
+
+def menu():
+    print("\n ===== Cadastro de Usuários =====")
+    print("1 - Cadastrar Usuário")
+    print("2 - Listar Usuários")
+    print("3 - Alterar Usuários")
+    print("4 - Remover Usuários")
+    print("5 - Sair do Programa")
